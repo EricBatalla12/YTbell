9ec89d036c59b85a36268308d2bd07ab23157055
EricB <ebo12@gcloud.ua.es>
2026-03-25 23:42:13 +0100

    V1.0

---
+++ README.md
@@ -0,0 +1,43 @@
+# 🔔 YTbell
+**Your YouTube-to-Discord notifier.** Ping your server the second a new video drops. 
+
+![Discord](https://img.shields.io/badge/Discord-Bot-5865F2?style=flat&logo=discord&logoColor=white)
+![Python](https://img.shields.io/badge/Python-3.8+-blue?style=flat&logo=python&logoColor=white)
+![Open Source](https://img.shields.io/badge/Open_Source-Yes-success?style=flat)
+
+With **YTbell** you can notify your community the recent video of your *favorite* channel.
+
+## ✨ Features
+- ⚡ **Lightning Fast:** Uses RSS feeds to detect new videos almost instantly.
+- 🛡️ **Built-in Shorts Filter:** Automatically detects and ignores YouTube Shorts.
+- 🎯 **Smart Channel Detection:** Just type `channel link` and the bot will find the hidden channel ID for you.
+- 💻 **100% Open Source:** Host it yourself or use our public instance.
+- 🌈 **Customizable:** You can create a custom bot integrating YTbell features 
+
+---
+
+## 🚀 How to use YTbell
+
+### Option A: Invite the public bot (No coding required)
+Want YTbell in your server right now? Just click the link below to authorize the bot:
+> **[👉 Click here to invite YTbell to your Discord Server](https://discord.com/oauth2/authorize?client_id=1486040335716188192&permissions=8&integration_type=0&scope=bot)**
+
+### Option B: Host it yourself (For Developers)
+Want to create a custom version for your favorite YouTuber? Clone this repo and run it yourself!
+
+**1. Prerequisites:**
+You will need Python 3.8+ and a Discord Bot Token from the [Discord Developer Portal](https://discord.com/developers/applications).
+*⚠️ Make sure to enable the **Message Content Intent** in the Bot tab!*
+
+**2. Installation:**
+```bash
+# Clone the repository
+git clone [https://github.com/YOUR_USERNAME/YTbell.git](https://github.com/YOUR_USERNAME/YTbell.git)
+cd YTbell
+
+# Install the required libraries
+pip install -r requirements.txt
+
+**3. Hosting:**
+**[Railway](https://railway.com/)** 
+**[Tutorial](https://www.figma.com/board/zZ7AVRpSNeVfhXknIk5fsv/Railway-tutorial?node-id=0-1&t=nsBlD8iNSfCQxBpt-1)**
--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import feedparser
 import requests
 import os
 import re
+import json
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
 
 load_dotenv()
 
 # ==========================================
-# CONSTANTES Y MEMORIA (Se borran al apagar el bot)
+# CONSTANTES Y MEMORIA
 # ==========================================
-URL_RSS_GENERIC = None
-canal_destino_id = None
-ultimo_enlace_conocido = None
-
+SERVER_LIST = "servers.json"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,6 +22,14 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # FUNCIONES
 # ==========================================
 def parse_channel_id(url):
+    if not url.startswith(("http://", "https://")) or not ("youtube.com" in url or "youtu.be" in url):
+        return "INVALID_URL"
+
+    # If url has the channel_id, direct match
+    direect_match = re.search(r"(UC[\w-]+)", url)
+    if direect_match:
+        return direect_match.group(1)
+    
     patron = r"channel_id=(UC[\w-]+)" #Expresión regular
 
     respuesta = requests.get(url, timeout = 10)
@@ -34,6 +40,18 @@ def parse_channel_id(url):
         return channel_id.group(1)
     else:
         return None
+
+#Function that load an entire json archive where are saved x servers
+def data_loader():
+    if os.path.exists(SERVER_LIST):
+        with open(SERVER_LIST, "r") as archive:
+            return json.load(archive)
+    else:
+        return {}
+
+def data_saver(data):
+    with open(SERVER_LIST, "w") as archive: # "w" erase the entire archive
+        json.dump(data, archive, indent = 4)
 
 def es_short(video_id):
     url_prueba = f"https://www.youtube.com/shorts/{video_id}"
@@ -51,61 +69,71 @@ def es_short(video_id):
 @bot.event
 async def on_ready():
     print(f'El bot: {bot.user} está en funcionamiento')
-    if not vigilar_youtube.is_running():
-        vigilar_youtube.start()
+    if not yt_watcher.is_running():
+        yt_watcher.start()
 
 @tasks.loop(minutes=10)
-async def vigilar_youtube():
-    global ultimo_enlace_conocido 
-    global URL_RSS_GENERIC
-    global canal_destino_id
+async def yt_watcher():
     
-    # Si no nos han dado una URL o un canal de Discord, no hacemos nada y esperamos
-    if URL_RSS_GENERIC is None or canal_destino_id is None:
-        return
-    #Diccionario clave-valor     
-    feed = feedparser.parse(URL_RSS_GENERIC)
-    #feed.entries devuelve los últimos 15 videos más recientes
-    if feed and len(feed.entries) > 0:
-        for video in feed.entries:
-            video_id = video.yt_videoid
-            
-            if not es_short(video_id):
-                link_actual = video.link
-                titulo_actual = video.title
+    data = data_loader()
+    for guild_id in data:
+        dst_channel_id = data[guild_id].get("dst_channel_id")
+        rss = data[guild_id].get("rss")
+        last_link_know = data[guild_id].get("last_link_know")
+        if dst_channel_id is None or rss is None:
+            continue #Next guild_id
+        #Diccionario clave-valor     
+        feed = feedparser.parse(rss)
+        #feed.entries devuelve los últimos 15 videos más recientes
+        if feed and len(feed.entries) > 0:
+            for video in feed.entries:
+                video_id = video.yt_videoid
                 
-                if ultimo_enlace_conocido is None:
-                    ultimo_enlace_conocido = link_actual
-                    print(f"Sistema inicializado. Último VÍDEO en memoria: {titulo_actual}")
+                if not es_short(video_id):
+                    actuall_link = video.link
+                    actuall_title = video.title
                     
-                elif link_actual != ultimo_enlace_conocido:
-                    canal_destino = bot.get_channel(canal_destino_id)
-                    if canal_destino:
-                        try:
-                            await canal_destino.send(f"¡NUEVO VÍDEO DETECTADO!\n**{titulo_actual}**\n{link_actual}", silent=True)
-                        except Exception as e:
-                            print(f"Error al enviar el mensaje: {e}")
-                            
-                    ultimo_enlace_conocido = link_actual
-                else:
-                    print("Chequeo rutinario: No hay vídeos nuevos.")
-                
-                break 
+                    if last_link_know is None:
+                        last_link_know = actuall_link
+                        data[guild_id]["last_link_know"] = last_link_know
+                        data_saver(data)
+                        print(f"Sistema inicializado. Último VÍDEO en memoria: {actuall_title}")
+                        
+                    elif actuall_link != last_link_know:
+                        channel_sender = bot.get_channel(dst_channel_id)
+                        if channel_sender:
+                            try:
+                                await channel_sender.send(f"¡NUEVO VÍDEO DETECTADO!\n**{actuall_title}**\n{actuall_link}", silent=True)
+                            except Exception as e:
+                                print(f"Error al enviar el mensaje: {e}")
+                                
+                        last_link_know = actuall_link
+                        data[guild_id]["last_link_know"] = last_link_know
+                        data_saver(data)
+                    else:
+                        print("Chequeo rutinario: No hay vídeos nuevos.")
+                    
+                    break 
+
+    
 
 # ==========================================
 # COMANDOS MANUALES
 # ==========================================
 
 @bot.command()
-async def generic(ctx):
-    global URL_RSS_GENERIC
+async def lv(ctx):
+    data = data_loader()
+    guild_id = str(ctx.guild.id)
     
-    if URL_RSS_GENERIC is None:
-        await ctx.send("¡Aún no me has dicho qué canal vigilar! Usa `!setytchannel <url>` primero.", silent=True)
+    if guild_id not in data or data[guild_id].get("rss") is None:
+        await ctx.send("Use `!setytchannel <url>` first.", silent=True)
         return
 
-    await ctx.send("Buscando el último vídeo...", silent=True)
-    feed = feedparser.parse(URL_RSS_GENERIC)
+    rss = data[guild_id].get("rss")
+
+    await ctx.send("Searching the lastest video...", silent=True)
+    feed = feedparser.parse(rss)
     
     if feed and len(feed.entries) > 0:
         for video in feed.entries:
@@ -122,27 +150,59 @@ async def generic(ctx):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def setytchannel(ctx, url_canal: str):
-    global URL_RSS_GENERIC
-    id_del_canal = parse_channel_id(url_canal)
-    print(f"ID: {id_del_canal}")
-    # El bot construye la URL completa automáticamente juntando la base y la ID que le des
-    URL_RSS_GENERIC = f"https://www.youtube.com/feeds/videos.xml?channel_id={id_del_canal}"
     
-    await ctx.send(f"✅ Canal enlazado. URL construida y guardada en memoria: {URL_RSS_GENERIC}")
+    guild_id = str(ctx.guild.id)
+
+    await ctx.send("Searching...", silent=True)
+    yt_id = parse_channel_id(url_canal)
+    
+    if yt_id == "INVALID_URL":
+        await ctx.send("❌Invalid url❌", silent = True)
+        return
+    
+    if yt_id is None:
+        await ctx.send("❌ I couldn't find the ID for that YT channel. Please make sure you provide a valid link.", silent=True)
+        return
+
+    data = data_loader()
+
+    if guild_id not in data:
+        data[guild_id] = {}
+
+    rss = f"https://www.youtube.com/feeds/videos.xml?channel_id={yt_id}"
+
+    if data[guild_id].get("rss") == rss:
+        await ctx.send("This YT channel is already configurated to send notifications", silent = True) 
+        return
+
+    data[guild_id]["rss"] = rss
+    data_saver(data)
+    await ctx.send(f"✅ YT channel linked succefully", silent=True)
+    print(f"New URL RSS: {rss}")
+
+@setytchannel.error
+async def setytchannel_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("❌ Necesitas permisos de Administrador para usar este comando.", silent=True)
 
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def setcanal(ctx):
-    global canal_destino_id
+    guild_id = str(ctx.guild.id)
+    channel_id = ctx.channel.id
 
-    if canal_destino_id == ctx.channel.id:
-        await ctx.send("Este canal ya está notificado")
+    data = data_loader()
+
+    if guild_id not in data: #First time using bot in this guild
+        data[guild_id] = {} 
+    
+    if data[guild_id].get("dst_channel_id") == channel_id: #This channel is already configured
+        await ctx.send(f"{ctx.channel.mention} is already configured")
         return
 
-    # Guardamos el canal de Discord en la memoria global
-    canal_destino_id = ctx.channel.id
-    
-    await ctx.send(f"✅ Las alertas de vídeos llegarán a {ctx.channel.mention}", silent=True)
+    data[guild_id]["dst_channel_id"] = channel_id #RAM memory
+    data_saver(data) #Really saved
+    await ctx.send(f"{ctx.channel.mention} is ready to recieve notifications ✅", silent = True)
 
 @setcanal.error
 async def setcanal_error(ctx, error):
